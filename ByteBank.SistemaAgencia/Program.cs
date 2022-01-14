e96b7f9ec14bb77d1ce51337c345e583734cf771
Italo <italo.avelino@outlook.com.br>
2022-01-14 14:34:57 -0300

    Criando projetos do banco ByteBank

---
+++ ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+using Humanizer;
+
+namespace ByteBank.SistemaAgencia
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dataFimPagamento = new DateTime(2022, 8, 17);
+            DateTime dataCorrente = DateTime.Now;
+
+            TimeSpan diferenca = TimeSpan.FromMinutes(60);
+
+            string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+
+            Console.WriteLine(mensagem);
+            
+            Console.ReadLine();
+        }
+
+    }
+}
+ 
+ 
