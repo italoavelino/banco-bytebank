bbd1ec6cf0e69646be15af43328ff45d91fb32ab
Italo Fernandes <italo.avelino@outlook.com.br>
2022-01-17 02:11:43 -0300

    Correção de conflito

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -9,19 +9,13 @@ using Humanizer;
 
 namespace ByteBank.SistemaAgencia
 {
-     class Program
+    class Program
     {
         static void Main(string[] args)
         {
             DateTime dataFimPagamento = new DateTime(2022, 7, 17);
             DateTime dataCorrente = DateTime.Now;
-
-<<<<<<< HEAD
-            string teste = "Teste de conflitos no git";
-=======
-            string teste = "Testando Conflitos no Git";
->>>>>>> cdfb7fab7f5f104f0083b190f3bd22263f1a1481
-
+            
             TimeSpan diferenca = dataCorrente - dataFimPagamento;
 
             string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
@@ -33,5 +27,4 @@ namespace ByteBank.SistemaAgencia
         }
     }
 }
- 
- 
+
